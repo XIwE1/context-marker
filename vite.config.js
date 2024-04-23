0da0aa76182c5877f258f30bd8d36eb6a8f1c6a0
wenyuan <804393870@qq.com>
2024-04-23 22:14:46 +0800

    build:打包加入eslint

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,38 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
-import legacy from '@vitejs/plugin-legacy';
+import legacy from "@vitejs/plugin-legacy";
+import eslint from 'vite-plugin-eslint'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   server: {
-    host: '10.1.7.44',
-    port: 8300,
-    open: true,
-    strictPort: true,
-    cors: true,
-    hmr: true
+    // host: "10.1.7.44",
+    // port: 8302,
+    // open: true,
+    // strictPort: true,
+    // cors: true,
+    // hmr: true,
   },
   build: {
-    outDir: 'lib',
-    target: 'es2015',
+    outDir: "lib",
+    target: "es2015",
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      fileName: 'index',
-      formats: ['es', 'iife', 'umd'],
-      name: 'ContextMarker',
-    }
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "iife", "umd"],
+      name: "ContextMarker",
+    },
   },
   plugins: [
-    // babel(),
+    babel(),
+    // {
+    //   apply: "build",
+    //   ...eslint(),
+    // },
+    dts(),
     // legacy({
     //   targets: ['> 1%', 'not IE 11'],
     // })
   ],
-})
+});
